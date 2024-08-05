f0438b86e865c11e4b77cef9fdc8ef8a4bd0c0f1
Merna Islam <101946404+mernaislam@users.noreply.github.com>
2024-08-05 23:59:28 +0300

    feat: Add light/dark themes, colors and fonts

--- lib/core/themes/app_themes.dart
+++ lib/core/themes/app_themes.dart
@@ -1,18 +1,35 @@
 import 'package:evento_app/core/themes/app_colors.dart';
+import 'package:evento_app/core/themes/custom_text_theme.dart';
 import 'package:flutter/material.dart';
 
 // to be completed
 class AppThemes {
   static var lightTheme = ThemeData(
-    scaffoldBackgroundColor: AppColors.white,
+    useMaterial3: true,
+    brightness: Brightness.light,
+    scaffoldBackgroundColor: AppColors.lightBackground,
+    secondaryHeaderColor: AppColors.primaryColor.withOpacity(0.1),
+    textTheme: CustomTextTheme.lightTextTheme,
+    iconTheme: const IconThemeData(
+      color: Colors.black,
+    ),
+    primaryColor: AppColors.primaryColor,
     appBarTheme: const AppBarTheme(
-      color: AppColors.olive,
+      color: AppColors.lightBackground,
     ),
   );
   static var darkTheme = ThemeData(
-    scaffoldBackgroundColor: AppColors.olive,
+    useMaterial3: true,
+    brightness: Brightness.dark,
+    scaffoldBackgroundColor: AppColors.darkBackground,
+    secondaryHeaderColor: AppColors.primaryColor.withOpacity(0.1),
+    textTheme: CustomTextTheme.darkTextTheme,
+    iconTheme: const IconThemeData(
+      color: Colors.white,
+    ),
+    primaryColor: AppColors.primaryColor,
     appBarTheme: const AppBarTheme(
-      color: AppColors.lightGreen,
+      color: AppColors.darkBackground,
     ),
   );
 }
