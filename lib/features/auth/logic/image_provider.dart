56d8ab837c58525493e43b2ebea630258cd0dc4c
Merna Islam <101946404+mernaislam@users.noreply.github.com>
2024-08-11 15:06:59 +0300

    fix: change return value of provider

--- lib/features/auth/logic/image_provider.dart
+++ lib/features/auth/logic/image_provider.dart
@@ -2,4 +2,4 @@ import 'dart:io';
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-final imageProvider = StateProvider<File?>((ref) => File('')); 
+final imageProvider = StateProvider<File?>((ref) => null); 
