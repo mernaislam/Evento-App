3ad0f97d597f221df15d4b6c0a882270248ff470
Nardeen <nardeen.hany.intern@linkdevelopment.com>
2024-08-12 23:54:21 +0300

    integrated with database to display real events creaed by this user instead of static data

--- lib/features/my_events/ui/widgets/events_gridview.dart
+++ lib/features/my_events/ui/widgets/events_gridview.dart
@@ -1,13 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:evento_app/features/my_events/ui/widgets/event_card.dart';
+import 'package:evento_app/features/event_integration/data/models/event_model.dart';
 
 class EventsGridView extends StatelessWidget {
   const EventsGridView({
     super.key,
-    // required this.events,
+    required this.events,
   });
 
-  // final List<Event> events;
+  final List<Event> events;
   @override
   Widget build(BuildContext context) {
     return SizedBox(
@@ -17,13 +18,13 @@ class EventsGridView extends StatelessWidget {
           crossAxisCount: 2,
           childAspectRatio: 0.7,
         ),
-        itemCount: 10, //change to events.length
+        itemCount: events.length, //change to events.length
         itemBuilder: (context, index) {
           return SizedBox(
             height: MediaQuery.of(context).size.height * 0.3,
             width: MediaQuery.of(context).size.width * 0.45,
-            child: const EventCard(isCreator: true),
-          ); //change to EventCard(event: events[index])
+            child: EventCard(isCreator: true, event: events[index]),
+          );
         },
       ),
     );
